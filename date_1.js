5746f5fa596e46b588765d846da1dd64a8165f7a
armeagle777 <tyeranyan@gmail.com>
2021-12-22 21:59:00 +0400

    Task: month, between two given dates, Resolved

--- date_1.js
+++ date_1.js
@@ -2,19 +2,33 @@
 
 
 
-function monthsInterval(start, end){
+function monthsInterval(startDate, endDate){
+  const start = startDate.getMonth()
+  const end = endDate.getMonth() > start ? endDate.getMonth() : endDate.getMonth() + 12
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let [startMonth,endMonth] = [Math.min(start.getMonth(), end.getMonth()),  Math.max(start.getMonth(), end.getMonth())];
+
   let result = [];
-  for(let i = (startMonth + 1); i <= endMonth; i++ ){
-    result.push(months[new Date(2017, i, 1).getMonth()])
+  for(let i =  start; i <= end && result.length < 12; i++ ){
+    let j = i <= 11 ? i : i-12
+    result.push(j)
   }
-  return result;
+  return result.sort((a,b) => a-b).map(monthIndex => months[monthIndex]);
 }
 
 
 
+// Testing for some cases
+
+let startDate = new Date(2017, 0, 1);
+let endDate = new Date(2017, 2, 1);
+console.log( monthsInterval( startDate, endDate ) );
+
+startDate = new Date(2017, 11, 1);
+endDate = new Date(2018, 0, 1);
+console.log( monthsInterval( startDate, endDate ) );
+
+startDate = new Date(2017, 0, 1);
+endDate = new Date(2018, 0, 1);
+console.log( monthsInterval( startDate, endDate ) );
 
 
-let jan = new Date(2017, 0, 1);
-let dec = new Date(2017, 11, 1);
